529efecdf385f3ebf5ee5e6cfd0aa8ea1d2f6dee
KirkStarter <144957396+KirkStarter@users.noreply.github.com>
2023-09-14 00:36:13 +0300

    Add files via upload

---
+++ RPS2.0_MOD(with_comp).rb
@@ -0,0 +1,31 @@
+# Масив можливих варіантів
+valid_options = ["rock", "sciss", "paper"]
+
+loop do
+
+  puts "Enter your choice: "
+  player_choice = gets.chomp.downcase
+
+  if valid_options.include?(player_choice)
+    computer_choice = valid_options.sample
+    puts "Your computer has selected #{computer_choice}."
+
+    if player_choice == computer_choice
+      puts "Draw! Let's shake the mouse :-)"
+
+    elsif (player_choice == "rock" && computer_choice == "sciss") ||
+      (player_choice == "sciss" && computer_choice == "paper") ||
+      (player_choice == "paper" && computer_choice == "rock")
+      puts "You win!#{player_choice.capitalize} beats #{computer_choice}."
+
+    else
+      puts "You lose!#{computer_choice.capitalize} beats #{player_choice}."
+    end
+
+    break
+
+  else
+    puts "Wrong choice. Valid options: #{valid_options.join(', ')}."
+  end
+
+end
